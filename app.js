235e21f3fabc7d67b91adccda174f8cd1c223146
davidrs <david.rs@hotmail.com>
2015-01-15 20:32:48 -0800

    Improve style and layout

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var app = {
 
       app.loadCastData().then(function(data){
         console.log('data', data);
-        app.chartVertical(data,'movie_name');
-        //charts.drawAllMoviesAllCast(data,"department", "Full Credits By Department");
+        app.chartVertical(data,'movie_name', true);
+        charts.drawAllMoviesAllCast(data,"department", "All Movies By Department", false);
       });
     });
   },
 
-  chartVertical: function(data, nestKey){
-      charts.drawAllMoviesAllCast(data,nestKey, "Full Credits By "+nestKey);
-      charts.drawAllMoviesOnlyActors(data, nestKey);
-      charts.drawAllMoviesOnlyCrew(data, nestKey);
+  chartVertical: function(data, nestKey, SHOW_IMAGES){
+      charts.drawAllMoviesAllCast(data,nestKey, "Full Credits By "+nestKey, SHOW_IMAGES);
+      charts.drawAllMoviesOnlyActors(data, nestKey, SHOW_IMAGES);
+      charts.drawAllMoviesOnlyCrew(data, nestKey, SHOW_IMAGES);
   },
 
   loadCastData: function(){
@@ -34,21 +34,21 @@ app.start();
 var charts = {
   tooltip: null,
 
-  drawAllMoviesOnlyActors: function(data, nestKey){
+  drawAllMoviesOnlyActors: function(data, nestKey, SHOW_IMAGES){
     data = _(data).filter(function(d){
       return d.department === "actor";
     });
-    charts.drawAllMoviesAllCast(data,nestKey, "Actors Only By "+ nestKey);
+    charts.drawAllMoviesAllCast(data,nestKey, "Actors Only By "+ nestKey, SHOW_IMAGES);
   },
 
-  drawAllMoviesOnlyCrew: function(data, nestKey){
+  drawAllMoviesOnlyCrew: function(data, nestKey, SHOW_IMAGES){
     data = _(data).filter(function(d){
       return d.department !== "actor";
     });
-    charts.drawAllMoviesAllCast(data, nestKey, "Crew Only By "+ nestKey);
+    charts.drawAllMoviesAllCast(data, nestKey, "Crew Only By "+ nestKey, SHOW_IMAGES);
   },
 
-  drawAllMoviesAllCast: function(data,  nestKey, title){
+  drawAllMoviesAllCast: function(data,  nestKey, title, SHOW_IMAGES){
     // use 'nest' to get total number of male, female, and unkown.
     var nestData = d3.nest()
       .key(function(credit){ return credit[nestKey]; })
@@ -63,12 +63,12 @@ var charts = {
       })
       .entries(data);
 
-      charts.drawChart(nestData, title);
+      charts.drawChart(nestData, title, SHOW_IMAGES);
   },
 
-  drawChart: function(data, title){
-    var WIDTH = 700,
-        HEIGHT = 700,
+  drawChart: function(data, title, SHOW_IMAGES){
+    var WIDTH = 600,
+        HEIGHT = 600,
         PADDING = 40,
         xKey = 'male',
         yKey = 'female',
@@ -82,6 +82,7 @@ var charts = {
 
     var svg = d3.select('body').append('svg').attr('width', WIDTH).attr('height',HEIGHT);
     charts.addImageDefs(svg, data, yKey);
+
     // add tooltip singelton if needed
     if(!charts.tooltip){
       charts.tooltip = d3.select("body").append("div")
@@ -89,25 +90,22 @@ var charts = {
       .style("opacity", 0);
     }
 
-
     var circles = svg.selectAll('g').data(data).enter().append('g').append('circle')
       .attr('cx', function(d,i){
         return x(d.values[xKey]);
       }).attr('cy', function(d,i){
         return y((d.values[yKey] ? d.values[yKey] : 0 ));
       }).attr('r', function(d,i){
-        return 25;//+Math.sqrt(d.values[sizeKey]);
+        return (SHOW_IMAGES ? 25 : Math.sqrt(d.values[sizeKey]));
       }).style('fill', function(d,i){
         var patternId = d.values.movie_id+('-'+Math.random()).substr(2,6);
         charts.createPattern(svg, d, x, y, xKey, yKey, patternId);
-        return  "url(#"+patternId+")";
-        //return colors(d.key);
+        return  (SHOW_IMAGES ? "url(#"+patternId+")" : colors(d.key));
       }).on("mouseover", function(d) {
           charts.tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          charts.tooltip.html(d.key + "<br/> " + d.values[yKey]
-          + " / " + d.values[xKey] + " female")
+          charts.tooltip.html('<strong>'+d.key + "</strong><br/> " + Math.round(100*d.values[yKey] / d.values.total) + "% female ")
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
       })
@@ -119,16 +117,10 @@ var charts = {
         .append("title")
           .text(function(d) {return d.key;});
 
-    // svg.selectAll('g').append("text")
-    //   .attr('x', function(d,i){
-    //     return x(d.values[xKey]);
-    //   }).attr('y', function(d,i){
-    //     return y((d.values[yKey] ? d.values[yKey] : 0 ));
-    //   })
-    //   .text(function(d){return ((d.values[yKey]/d.values[xKey])>0.4 ? d.key : '')});
 
     charts.drawAxis(svg, x, y,  HEIGHT, PADDING);
-    charts.drawTitle(svg, title, WIDTH);
+    charts.drawTitle(svg, title, WIDTH, HEIGHT, PADDING);
+    charts.drawMiddleLine(svg,x,y,WIDTH, HEIGHT, PADDING);
   },
 
   createPattern: function(svg, d, x, y, xKey, yKey, patternId){
@@ -154,30 +146,9 @@ var charts = {
   },
   addImageDefs: function(svg, data, yKey){
      var defs = svg.append('svg:defs').attr('class', 'defs');
-
-     // _(app.movieData).each(function(movie){
-     //  defs.append('svg:pattern')
-     //    .attr('id', movie.id)
-     //    .attr('patternUnits', 'userSpaceOnUse')
-     //            .attr('x', 0)
-     //    .attr('y', 0)
-     //    .attr('width', '130')
-     //    .attr('height', '73')
-     //    .append('svg:image')
-     //    .attr('xlink:href', "https://image.tmdb.org/t/p/w130/"+movie.backdrop_path)
-     //    .attr('x', 0)
-     //    .attr('y', function(){
-     //      var tmp = _(data).find(function(d){return d.values.movie_id == movie.id });
-     //      return (tmp ? tmp.values[yKey] : 0);
-     //    })
-     //    .attr('width', 130)
-     //    .attr('height', 73);
-     // });
-     //"https://image.tmdb.org/t/p/w342/"+movie.backdrop_path);
-     //"https://image.tmdb.org/t/p/w130/"+movie.poster_path);
   },
 
-  drawTitle: function(svg, title, WIDTH){
+  drawTitle: function(svg, title, WIDTH, HEIGHT, PADDING){
     svg.append("text")
       .attr("x", (WIDTH / 2))
       .attr("y", 16)
@@ -185,6 +156,20 @@ var charts = {
       .style("font-size", "16px")
       .style("text-decoration", "underline")
       .text(title);
+
+    svg.append("text")
+      .attr("x", 0)
+      .attr("y", PADDING)
+      .attr("text-anchor", "left")
+      .style("font-size", "11px")
+      .text("female");
+
+    svg.append("text")
+      .attr("x", WIDTH-PADDING)
+      .attr("y", HEIGHT-PADDING+5)
+      .attr("text-anchor", "left")
+      .style("font-size", "11px")
+      .text("male");
   },
 
   drawAxis: function(svg, x, y, HEIGHT, PADDING ){
@@ -202,5 +187,15 @@ var charts = {
                       .attr("class", "axis")
                       .attr("transform", "translate("+ PADDING +",0)")
                        .call(yAxis);
+  },
+
+  drawMiddleLine: function(svg, x, y,WIDTH, HEIGHT, PADDING){
+    svg.append('line')
+      .attr("x1",PADDING)
+      .attr("y1",HEIGHT-PADDING)
+      .attr("x2",WIDTH-PADDING)
+      .attr("y2", PADDING)
+      .attr("stroke-width", 1)
+      .attr("stroke", "purple");
   }
 };
